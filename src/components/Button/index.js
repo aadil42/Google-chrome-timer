0063980a5628661c8787f36c5beba93bd0143429
aadil42 <adilkelawala@gmail.com>
2024-11-06 03:36:37 +0530

    create btn

---
+++ src/components/Button/index.js
@@ -0,0 +1,12 @@
+
+const Button = ({styles, title}) => {
+    return (
+        <button
+        style={styles}
+        >
+            {title}
+        </button>
+    );
+}
+
+export default Button;
