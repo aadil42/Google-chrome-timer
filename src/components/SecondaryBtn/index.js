72bedb79850c0ddfb64e9dd2e6d1e38521d399fd
aadil42 <adilkelawala@gmail.com>
2024-11-06 03:46:11 +0530

    add  SecondaryBtn

---
+++ src/components/SecondaryBtn/index.js
@@ -0,0 +1,11 @@
+import Button from "../Button";
+
+const SecondaryBtn = ({title}) => {
+    return (
+        <Button 
+            title={title}
+        />
+    );
+}
+
+export default SecondaryBtn;
