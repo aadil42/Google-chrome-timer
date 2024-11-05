af6b75bddb7ed232b38a2154d2f215a65912b29b
aadil42 <adilkelawala@gmail.com>
2024-11-06 02:36:38 +0530

    add some constent

---
+++ src/CONST.js
@@ -0,0 +1,9 @@
+const CONST = {
+    POP_UP_WINDOW_HEIGHT: "400px",
+    POP_UP_WINDOW_WIDTH: "600px",
+    POP_UP_WINDOW_PRIMARY_BG: "#F24B6A",
+    POP_UP_WINDOW_SECONDARY_BG:  "#F27983" 
+}
+
+
+export default CONST;
