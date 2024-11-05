86e40c39740ec2dc6ef310183ea4046167590359
aadil42 <adilkelawala@gmail.com>
2024-11-06 03:46:29 +0530

    use PrimaryBtn

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import CONST from "./CONST";
 import Model from "./components/Model";
 import GradientDiv from "./components/GradientDiv";
 import MainTitle from "./components/MainTitle";
-import Button from "./components/Button";
+import PrimaryBtn from "./components/PrimaryBtn";
 
 import "./App.css";
 
@@ -25,23 +25,8 @@ function App() {
             paddingTop: '20px', 
           }}
           />
-
-          <Button 
-            title="btn" 
-            styles={{
-              display: 'block',
-              margin: '20px auto',
-              color: '#F24B6A',
-              backgroundColor: '#ffffff',
-              border: 'none',
-              borderRadius: '5px',
-              padding: '10px 20px',
-              textAlign: 'center',
-              cursor: 'pointer',
-              fontSize: '16px',
-              transition: 'background-color 0.3s',
-              fontWeight: 'bold',            
-            }}
+          <PrimaryBtn 
+          title={CONST.PRIMARY_BTN_TITLE}
           />
         </Model>
        </GradientDiv>
