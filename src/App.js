52d66ae1d8f22c4bfd7cf03ea0750d8b4fe27abf
aadil42 <adilkelawala@gmail.com>
2024-11-06 02:55:59 +0530

    use Modal and Gradient component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import CONST from "./CONST";
+import Model from "./components/Model";
+import GradientDiv from "./components/GradientDiv";
 
 function App() {
+
   return (
     <div className="App">
-
+      <Model 
+      width={CONST.POP_UP_WINDOW_WIDTH} 
+      height={CONST.POP_UP_WINDOW_HEIGHT}
+      >
+       <GradientDiv 
+       gradientColors={[CONST.POP_UP_WINDOW_PRIMARY_BG, CONST.POP_UP_WINDOW_SECONDARY_BG]}
+       >
+        
+       </GradientDiv>
+      </Model>
     </div>
   );
 }
